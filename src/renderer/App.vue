caf5a82617573c917396cb6c86e316a3ac65b199
gwangil <pki054@netman.co.kr>
2019-12-20 22:45:37 +0900

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vagrant-gui-tool'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
