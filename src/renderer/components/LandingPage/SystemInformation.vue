6666ad3ba52234e96de037c21f75c14b3d6d8c9b
gwangil <pki054@naver.com>
2020-02-10 08:24:58 +0900

    가상머신 동작하는 부분 변경 - MachineStatus -> SystemInformation

--- src/renderer/components/LandingPage/SystemInformation.vue
+++ src/renderer/components/LandingPage/SystemInformation.vue
@@ -1,6 +1,27 @@
   
 <template>
   <div>
+    <div>
+      <vnt-header>
+        {{value}}
+        <span slot="subheader">
+          {{vagrant_id}}
+        </span>
+      </vnt-header>
+
+      
+      <br>
+      
+    </div>
+    <div style="align-text:left;">
+      <md-card-actions>
+        <md-button v-on:click="configure(vagrant_id)">설정</md-button>
+        <md-button v-on:click="start(vagrant_id)">켜기</md-button>
+        <md-button v-on:click="stop(vagrant_id)">끄기</md-button>
+        <md-button v-on:click="reload(vagrant_id)">재시작</md-button>
+        <md-button v-on:click="remove(vagrant_id)">삭제</md-button>
+      </md-card-actions>
+    </div>
     <div>Snapshot Lists</div>
     <pre>
       <li v-for="key in SnapshotList.slice(1)">{{key}}</li>
@@ -10,17 +31,108 @@
 
 <script>
   import EventBus from '../../store/eventBus'
+  var spawn = require('child_process').spawn
+  var exec = require('child_process').exec
 
   export default {
     name: 'menu',
+    props: ['value', 'vagrant_id'],
     data () {
       return {
         SnapshotList: []
       }
     },
+    methods: {
+      remove: function (id) {
+        var child = spawn('vagrant', ['destroy', '-f', id])
+
+        child.stdout.on('data', (data) => {
+          var getId = document.getElementById(id)
+          getId.style.backgroundColor = 'green'
+          getId.style.color = 'white'
+          EventBus.$emit('addLogger', data)
+          EventBus.$emit('removeVM', id)
+        })
+        child.stderr.on('data', (data) => {
+          var getId = document.getElementById(id)
+          getId.style.backgroundColor = 'red'
+          getId.style.color = 'white'
+          EventBus.$emit('addLogger', data)
+        })
+      },
+      reload: function (id) {
+        exec('vagrant reload ' + id, function (error, stdout, stderr) {
+          var getId = document.getElementById(id)
+          EventBus.$emit('addLogger', stdout)
+          console.log('stdout: ' + stdout)
+          console.log('stderr: ' + stderr)
+
+          getId.style.backgroundColor = 'green'
+          getId.style.color = 'white'
+
+          if (error !== null) {
+            getId.style.backgroundColor = 'red'
+            EventBus.$emit('addLogger', stderr)
+
+            console.log('exec error: ' + error)
+          }
+        })
+      },
+      start: function (id) {
+        var child = spawn('vagrant', ['up', id])
+
+        child.stdout.on('data', (data) => {
+          var getId = document.getElementById(id)
+          getId.style.backgroundColor = 'green'
+          getId.style.color = 'white'
+          EventBus.$emit('addLogger', data)
+        })
+        child.stderr.on('data', (data) => {
+          var getId = document.getElementById(id)
+          getId.style.backgroundColor = 'red'
+          getId.style.color = 'white'
+          EventBus.$emit('addLogger', data)
+        })
+        // exec('vagrant up ' + id, function (error, stdout, stderr) {
+        //   var getId = document.getElementById(id)
+        //   EventBus.$emit('addLogger', stdout)
+        //   console.log('stdout: ' + stdout)
+        //   console.log('stderr: ' + stderr)
+
+        //   getId.style.backgroundColor = 'green'
+        //   getId.style.color = 'white'
+
+        //   if (error !== null) {
+        //     getId.style.backgroundColor = 'red'
+        //     EventBus.$emit('addLogger', stderr)
+
+        //     console.log('exec error: ' + error)
+        //   }
+        // })
+      },
+      stop: function (id) {
+        exec('vagrant halt ' + id, function (error, stdout, stderr) {
+          var getId = document.getElementById(id)
+          EventBus.$emit('addLogger', stdout)
+
+          console.log('stdout: ' + stdout)
+          console.log('stderr: ' + stderr)
+          getId.style.backgroundColor = 'white'
+          getId.style.color = 'black'
+          if (error !== null) {
+            getId.style.backgroundColor = 'red'
+            console.log('exec error: ' + error)
+          }
+        })
+      }
+    },
     created () {
       EventBus.$on('addSnapshot', (payload) => {
         this.SnapshotList = payload.split('\n')
+      })
+      EventBus.$on('setInform', (payload, id) => {
+        this.value = payload
+        this.vagrant_id = id
       })
     }
   }
--- src/renderer/components/assets/MachineStatus.vue
+++ src/renderer/components/assets/MachineStatus.vue
@@ -1,19 +1,16 @@
   
 <template>
   <div>
-    <md-card class="md-primary" :style="value.state" :id="vagrant_id" style="width:100%;">
+    <md-card class="md-primary" :style="value.state" :id="vagrant_id">
       <md-card-header>
         <md-card-header-text>
-          <div>{{value.local_data_path}}</div>
-          <div class="md-subhead">{{vagrant_id}}</div>
+          <div v-on:click="inform(value.local_data_path, vagrant_id)">
+            <div>{{value.local_data_path}}</div>
+            <div class="md-subhead">{{vagrant_id}}</div>
+          </div>
         </md-card-header-text>
       </md-card-header>
-      <!-- <md-card-actions>
-        <md-button v-on:click="configure(vagrant_id)">설정</md-button>
-        <md-button v-on:click="start(vagrant_id)">켜기</md-button>
-        <md-button v-on:click="stop(vagrant_id)">끄기</md-button>
-        <md-button>재시작</md-button>
-      </md-card-actions> -->
+    
     </md-card>
   </div>
 </template>
@@ -31,39 +28,8 @@ export default {
   props: ['value', 'vagrant_id'],
   // 메소드는 `methods` 객체 안에 정의합니다
   methods: {
-    start: function (id) {
-      child = exec('vagrant up ' + id, function (error, stdout, stderr) {
-        var getId = document.getElementById(id)
-
-        EventBus.$emit('addLogger', stdout)
-        console.log('stdout: ' + stdout)
-        console.log('stderr: ' + stderr)
-
-        getId.style.backgroundColor = 'green'
-        getId.style.color = 'white'
-
-        if (error !== null) {
-          getId.style.backgroundColor = 'red'
-          EventBus.$emit('addLogger', stderr)
-
-          console.log('exec error: ' + error)
-        }
-      })
-    },
-    stop: function (id) {
-      child = exec('vagrant halt ' + id, function (error, stdout, stderr) {
-        var getId = document.getElementById(id)
-        EventBus.$emit('addLogger', stdout)
-
-        console.log('stdout: ' + stdout)
-        console.log('stderr: ' + stderr)
-        getId.style.backgroundColor = 'white'
-        getId.style.color = 'black'
-        if (error !== null) {
-          getId.style.backgroundColor = 'red'
-          console.log('exec error: ' + error)
-        }
-      })
+    inform: function (name, id) {
+      EventBus.$emit('SetSystemInformation', name, id)
     },
     configure: function (id) {
       child = exec('vagrant snapshot list ' + id, function (error, stdout, stderr) {
