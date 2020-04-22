367620a2f56f3695e332940e6310702c44f804d7
gwangil <pki054@naver.com>
2020-04-22 10:51:54 +0900

    기존 소스 제거 (리팩토링)

--- src/utils/config.js
+++
@@ -1,9 +0,0 @@
-const storage = require('electron-json-storage')
-var config = {}
-const dataPath = `${storage.getDataPath()}/Vagrantable`
-
-config.getPath = function (arg1, arg2) {
-  console.log(`${dataPath}`)
-}
-
-module.exports = config
