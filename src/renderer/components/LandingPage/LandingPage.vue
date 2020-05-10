a26c1124764f2bc2e850cb0589c9c14ec2285339
gwangil <pki054@naver.com>
2020-05-10 21:59:23 +0900

    hr 태그 제거

--- src/renderer/components/LandingPage/LandingPage.vue
+++ src/renderer/components/LandingPage/LandingPage.vue
@@ -4,7 +4,6 @@
       <menu-list style="height:100%;"></menu-list>
       <div style="margin-top:2%;width:90%; height:100%;z-index: 1;">
         <div v-bind:vagrant_id="vagrantId" v-bind:vagrant_name="vagrantName" class="overflow-y-auto" style="overflow:auto;height:70vh;width:100%;" :is="currentComponent" :swap-component="swapComponent"></div>
-        <hr>
         <v-card ref="content" class="overflow-y-auto" style="height:20%; width:100%;overflow-y:scroll;bottom:20px;position: fixed;" id="logger">
           <pre  wrap="hard">
             {{Logger}}
